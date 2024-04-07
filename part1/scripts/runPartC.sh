bf3b60a0497a7939c97d3c2712fa1541089e076e
SaiMadhavanG <g.saimadhavan@gmail.com>
2024-04-07 13:15:45 +0530

    added instructions for problem 1_c

--- part1/scripts/runPartC.sh
+++ part1/scripts/runPartC.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Processing d=1"
-hadoop jar Strips.jar CoocurenceStrips Wikipedia-EN-20120601_ARTICLES strips10kd1 top50.txt -d 1 > strips10kd1.log 2>&1
+hadoop jar ./jars/Strips.jar CoocurenceStrips ./data/Wikipedia-EN-20120601_ARTICLES ./outputs/strips10kd1 top50.txt -d 1 > ./logs/strips10kd1.log 2>&1
 echo "Processing d=2"
-hadoop jar Strips.jar CoocurenceStrips Wikipedia-EN-20120601_ARTICLES strips10kd2 top50.txt -d 2 > strips10kd2.log 2>&1
+hadoop jar ./jars/Strips.jar CoocurenceStrips ./data/Wikipedia-EN-20120601_ARTICLES ./outputs/strips10kd2 top50.txt -d 2 > ./logs/strips10kd2.log 2>&1
 echo "Processing d=3"
-hadoop jar Strips.jar CoocurenceStrips Wikipedia-EN-20120601_ARTICLES strips10kd3 top50.txt -d 3 > strips10kd3.log 2>&1
+hadoop jar ./jars/Strips.jar CoocurenceStrips ./data/Wikipedia-EN-20120601_ARTICLES ./outputs/strips10kd3 top50.txt -d 3 > ./logs/strips10kd3.log 2>&1
 echo "Processing d=4"
-hadoop jar Strips.jar CoocurenceStrips Wikipedia-EN-20120601_ARTICLES strips10kd4 top50.txt -d 4 > strips10kd4.log 2>&1
+hadoop jar ./jars/Strips.jar CoocurenceStrips ./data/Wikipedia-EN-20120601_ARTICLES ./outputs/strips10kd4 top50.txt -d 4 > ./logs/strips10kd4.log 2>&1
